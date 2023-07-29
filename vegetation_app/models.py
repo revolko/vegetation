383b57d36a8ff3a17d3d6114098f0d5d98880a86
Juraj Paluba <juraj.palubajp@gmail.com>
2023-07-29 18:49:03 +0200

    vegetation_app: create Plant model

--- vegetation_app/models.py
+++ vegetation_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1024)
+    last_watered = models.DateTimeField()
+    water_frequency = models.IntegerField()
