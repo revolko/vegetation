7b512c5ec4c7b9e0c5880346a9af3a3db0ef4eb7
Juraj Paluba <juraj.palubajp@gmail.com>
2023-08-02 21:17:43 +0200

    vegetation_app: update Plant model
    Added new attributes. The migrations are squashed, mainly because I
    wanted to try it out.

--- vegetation_app/models.py
+++ vegetation_app/models.py
@@ -5,4 +5,38 @@ class Plant(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1024)
     last_watered = models.DateTimeField()
-    water_frequency = models.IntegerField()
+    water_frequency_summer = models.IntegerField()
+    water_frequency_winter = models.IntegerField()
+
+    class WateringType(models.TextChoices):
+        SPRINKLER = 'SP', 'Sprinkler'
+        SAUCER = 'SA', 'Saucer'
+        PLANTER = 'PL', 'Planter'
+
+    watering_type = models.CharField(
+        max_length=2,
+        choices=WateringType.choices,
+        default=WateringType.SPRINKLER,
+    )
+
+    class DroughtTolerance(models.TextChoices):
+        LOW = 'L', 'Low'
+        MEDIUM = 'M', 'Medium'
+        HIGH = 'H', 'High'
+
+    drought_tolerance = models.CharField(
+        max_length=1,
+        choices=DroughtTolerance.choices,
+        default=DroughtTolerance.MEDIUM,
+    )
+
+    class LightRequirement(models.TextChoices):
+        LOW = 'L', 'Low'
+        MEDIUM = 'M', 'Medium'
+        HIGH = 'H', 'High'
+
+    light_requirement = models.CharField(
+        max_length=1,
+        choices=LightRequirement.choices,
+        default=LightRequirement.MEDIUM,
+    )
--- vegetation_app/serializers.py
+++ vegetation_app/serializers.py
@@ -6,4 +6,5 @@ from .models import Plant
 class PlantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plant
-        fields = ('id', 'name', 'description', 'last_watered', 'water_frequency')
+        fields = ('id', 'name', 'description', 'last_watered', 'water_frequency_summer', 'water_frequency_winter',
+                  'watering_type', 'drought_tolerance', 'light_requirement')
