d53fc9b99dcfbecd7d429d9d9b6d296b1ee7228b
Juraj Paluba <juraj.palubajp@gmail.com>
2023-11-04 21:22:21 +0100

    vegetation_app: secure endpoints
    Only Authenticated (Authorization Token ... header) users can access
    vegetation app.

--- vegetation_app/views.py
+++ vegetation_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Plant
 from .serializers import PlantSerializer
@@ -7,8 +8,10 @@ from .serializers import PlantSerializer
 class PlantList(ListCreateAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class PlantDetail(RetrieveUpdateDestroyAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+    permission_classes = [IsAuthenticated]
