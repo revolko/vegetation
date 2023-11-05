11dc520bfd77251c2ca6f0210ac69aaf5a4b53ea
Juraj Paluba <juraj.palubajp@gmail.com>
2023-11-05 17:22:31 +0100

    project: load .env variables to environment

--- vegetation/settings.py
+++ vegetation/settings.py
@@ -9,22 +9,22 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from pathlib import Path
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
-
-CONFIG_ENV = dotenv_values(".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = CONFIG_ENV.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = CONFIG_ENV.get('DEBUG')
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'vegetation.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': CONFIG_ENV.get('POSTGRES_DB'),
-        'USER': CONFIG_ENV.get('POSTGRES_USER'),
-        'PASSWORD': CONFIG_ENV.get('POSTGRES_PASSWORD'),
-        'HOST': CONFIG_ENV.get('POSTGRES_HOST'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
     }
 }
 
